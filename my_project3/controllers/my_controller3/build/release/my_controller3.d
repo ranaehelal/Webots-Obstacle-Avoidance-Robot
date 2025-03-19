049a4b32e5d4a55c04e66ab3c9c8ba041612e1a3
ranaehelal <115326882+ranaehelal@users.noreply.github.com>
2025-03-19 08:33:42 +0200

    Delete my_project3 directory

--- my_project3/controllers/my_controller3/build/release/my_controller3.d
+++
@@ -1,6 +0,0 @@
-build/release/my_controller3.o: my_controller3.cpp \
- /usr/local/webots/include/controller/cpp/webots/DistanceSensor.hpp \
- /usr/local/webots/include/controller/cpp/webots/Device.hpp \
- /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
- /usr/local/webots/include/controller/cpp/webots/Motor.hpp \
- /usr/local/webots/include/controller/cpp/webots/Robot.hpp
